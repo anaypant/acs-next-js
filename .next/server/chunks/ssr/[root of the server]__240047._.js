44671863a660cd5c445e52e8c2ffc573eca79c53
Utsav <utsiear@gmail.com>
2025-01-20 01:29:24 -0500

    tryna fix

--- .next/server/chunks/ssr/[root of the server]__240047._.js
+++ .next/server/chunks/ssr/[root of the server]__240047._.js
@@ -318,11 +318,6 @@ function AboutUs() {
                 fileName: "[project]/app/about/page.tsx",
                 lineNumber: 12,
                 columnNumber: 7
-            }, this),
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$15$2e$1$2e$4_react$2d$dom$40$19$2e$0$2e$0_react$40$19$2e$0$2e$0_$5f$react$40$19$2e$0$2e$0$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(Footer, {}, void 0, false, {
-                fileName: "[project]/app/about/page.tsx",
-                lineNumber: 50,
-                columnNumber: 7
             }, this)
         ]
     }, void 0, true, {
