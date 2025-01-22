8ef7798db440673cfc6f534c9aceda77bdda324f
anaypant <35899820+anaypant@users.noreply.github.com>
2025-01-22 00:10:08 -0500

    Delete .next directory

--- .next/server/app/api/contact-send-email/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/contact-send-email/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules__pnpm_6099cd._.js");
-runtime.loadChunk("server/chunks/[root of the server]__7ddc10._.js");
-runtime.loadChunk("server/chunks/_d785ec._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/contact-send-email/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.4_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/contact-send-email/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
