08cb5b9eb72444a64f0e4da7326be1732b909731
anaypant <35899820+anaypant@users.noreply.github.com>
2025-03-31 01:22:47 -0400

    fixed dev/prod supabase

--- app/utils/supabase/supabase.ts
+++ app/utils/supabase/supabase.ts
@@ -1,10 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const isDev = process.env.NODE_ENV === 'development';
+
+const SUPABASE_URL = isDev
+  ? process.env.NEXT_PUBLIC_SUPABASE_DEV_URL
+  : process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = isDev
+  ? process.env.NEXT_PUBLIC_SUPABASE_DEV_ANON_KEY
+  : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-    throw new Error('Missing Supabase URL or ANON KEY');
+  throw new Error('Missing Supabase URL or ANON KEY');
 }
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
